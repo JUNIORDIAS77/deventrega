acddc436a9e2f6ff4c0155603cea68a8f7e3e916
JAIR RODRIGUES <->
2022-12-24 11:14:17 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/deventrega/DeventregaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.deventrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeventregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeventregaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/deventrega/DeventregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.deventrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeventregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
